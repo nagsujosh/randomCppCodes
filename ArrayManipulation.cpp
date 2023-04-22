b15a637b73354cd4d3c0cac9b8c0c2821066fede
Sujosh Nag <116555835+nagsujosh@users.noreply.github.com>
2023-04-22 12:26:05 +0600

    Add files via upload

---
+++ ArrayManipulation.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e7+10;
+long long int arr[N];
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    while(m--){
+        int a, b, k;
+        cin >> a >> b >> k;
+        arr[a] += k;
+        arr[b + 1] -= k;
+    }
+    for(int i = 1; i <= n; i++){
+        arr[i] += arr[i - 1];
+    }
+    long long mx = -1;
+    for(int i = 1; i <= n; i++){
+        if(mx < arr[i]){
+            mx = arr[i];
+        }
+    }
+    cout << mx << endl;
+}
